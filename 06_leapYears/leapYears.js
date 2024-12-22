df83991c59a2aaeefd0d09231033c01e17b2a58a
Ryan Broda <brodaryan@gmail.com>
2024-12-22 12:44:00 -0500

    Completed exercise 6. Very excited to have goteen the exact same solution as TOP's solution.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let nonCenturyLeapYear = year % 4 == 0;
+  let centuryLeapYear = year % 100 !== 0 || year % 400 == 0;
 
+  if (nonCenturyLeapYear && centuryLeapYear) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
