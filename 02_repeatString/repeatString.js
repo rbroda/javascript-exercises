1a7c746ada5c1e66e1a4f13794e8b34c403293c7
Ryan Broda <brodaryan@gmail.com>
2024-12-21 23:32:13 -0500

    Completed exercises 1-3 with all tests passing.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, repeat) {
+  if (repeat < 0) {
+    return "ERROR";
+  } else if (repeat < 1) {
+    return "";
+  } else {
+    let repeatedString = "";
 
+    for (let i = 1; i <= repeat; i++) {
+      repeatedString = repeatedString.concat(string);
+    }
+
+    return repeatedString;
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringLength = string.length;
+  let reversedString = "";
 
+  for (let i = stringLength; i > 0; i--) {
+    properPosition = stringLength - 1;
+    removedLetter = string.charAt(properPosition);
+    reversedString = reversedString.concat(removedLetter);
+    stringLength = stringLength - 1;
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
