d0bc9e7bbb08c03a7eb890c93d06fc68370ef33c
Ryan Broda <brodaryan@gmail.com>
2024-12-22 11:58:58 -0500

    Completed exercises 4 and 5.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toBeRemoved) {
+  let newArray = [];
+
+  array.forEach((item) => {
+    if (!toBeRemoved.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let isNumber = typeof first === "number" && typeof second === "number";
+  let isInteger = Number.isInteger(first) && Number.isInteger(second);
+  let isNegative = first < 0 || second < 0;
 
+  if (!isNumber || !isInteger || isNegative) {
+    return "ERROR";
+  } else {
+    let arrayOfNumbers = [];
+    let sumOfNumbers = 0;
+
+    const getArrayOfNumbers = (first, second) => {
+      if (first > second) {
+        for (let i = second; i <= first; i++) {
+          arrayOfNumbers.push(i);
+        }
+      } else {
+        for (let i = first; i <= second; i++) {
+          arrayOfNumbers.push(i);
+        }
+      }
+    };
+    getArrayOfNumbers(first, second);
+
+    arrayOfNumbers.forEach((number) => {
+      sumOfNumbers = sumOfNumbers + number;
+    });
+
+    return sumOfNumbers;
+  }
 };
+
+sumAll(123, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
