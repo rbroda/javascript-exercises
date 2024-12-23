4ba0d17c25c7304f059cc7d2a05ba40a3cc9262c
Ryan Broda <brodaryan@gmail.com>
2024-12-22 20:13:37 -0500

    Completed exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreesFahrenheit) {
+  let degreesCelciusFull = ((degreesFahrenheit - 32) * 5) / 9;
+  let degreesCelciusRounded = degreesCelciusFull.toFixed(1);
+  return Number(degreesCelciusRounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesCelcius) {
+  let degreesFahrenheitFull = (degreesCelcius * 9) / 5 + 32;
+  let degreesFahrenheitRounded = degreesFahrenheitFull.toFixed(1);
+  return Number(degreesFahrenheitRounded);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
