82efdeb7018a481db93206e66099f413a2c45e96
Ryan Broda <brodaryan@gmail.com>
2025-10-28 02:47:47 -0400

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (arg) {
+  let count;
+  if (typeof arg !== "number") {
+    count = parseInt(arg);
+  } else {
+    count = arg;
+  }
 
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
+
+  let firstLast = 1;
+  let secondLast = 0;
+
+  for (let x = 2; x <= count; x++) {
+    let current = firstLast + secondLast;
+    secondLast = firstLast;
+    firstLast = current;
+  }
+
+  return firstLast;
 };
 
 // Do not edit below this line
