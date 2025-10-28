5a4147ee9279a65de26067cd2491ad584ca05b0f
Ryan Broda <brodaryan@gmail.com>
2025-10-28 02:26:49 -0400

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let left = 0;
+  let right = cleanedStr.length - 1;
 
+  while (left < right) {
+    if (cleanedStr[left] !== cleanedStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
 
 // Do not edit below this line
