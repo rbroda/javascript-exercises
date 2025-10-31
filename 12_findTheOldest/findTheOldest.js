f1aa13c250677874742a636bf70c3e0c32453ba0
Ryan Broda <brodaryan@gmail.com>
2025-10-31 19:59:24 -0400

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = (birthDate, deathDate) => {
+  if (!deathDate) {
+    deathDate = new Date().getFullYear();
+  }
+  return deathDate - birthDate;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currentAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
