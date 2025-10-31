087ea17040cd4ce7ce77de58294f082539cc8d4d
Ryan Broda <brodaryan@gmail.com>
2025-10-31 18:56:51 -0400

    completed getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let bookTitles = [];
 
+  books.forEach((book) => {
+    let bookTitle = book.title;
+
+    bookTitles.push(bookTitle);
+  });
+
+  return bookTitles;
 };
 
 // Do not edit below this line
